77ad13851c57ac4201f5b29b694ceadd853d4f5c
Amit Pal <amitpal16181@gmail.com>
2024-06-16 19:39:30 +0530

    Added signup and login feature

--- backend/models/user.js
+++ backend/models/user.js
@@ -1,24 +1,30 @@
 const mongoose = require('mongoose');
-const { UUID } = require('sequelize');
+const { UUID, UniqueConstraintError } = require('sequelize');
 const Schema = mongoose.Schema;
 
 const userSchema = new Schema({
-    _id:{
-        type : String,
-        required :true
+    _id: {
+        type: String,
+        required: true,
     },
-    username :{
-        type:String,
-        required : true,
+    name: {
+        type: String,
+        required: true
     },
-    password:{
-        type:String,
-        required : true,
+    username: {
+        type: String,
+        required: true,
     },
-    email:{
-        type:String,
-        required : true,
+    password: {
+        type: String,
+        required: true,
+    },
+    email: {
+        type: String,
+        required: true,
+        lowercase: true,
+        unique: true
     }
 
 })
-module.exports = mongoose.model('User',userSchema);
+module.exports = mongoose.model('User', userSchema);
--- backend/routes/userRoutes.js
+++ backend/routes/userRoutes.js
@@ -5,4 +5,5 @@ const userController = require("../controller/UserController");
 
 // userRouter.get('/fire', userController.ourTask);
 userRouter.post('/signup', userController.signup);
+userRouter.post('/login', userController.login);
 module.exports = userRouter;
